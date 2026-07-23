8c0c377434eff9215c476586363c1c95bb3d52a1
Smee18 <marcel.venturotti@icloud.com>
2026-07-23 16:36:23 +0200

    Autodoc integration

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,38 @@
 import os
 import sys
 
+project = "fbscatnet"
+copyright = "2026, Marcel Venturotti"
+author = "Marcel Venturotti"
+release = "0.1.0"
+
 sys.path.insert(0, os.path.abspath("../src"))
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",    # Supports Google and NumPy style docstrings
+    "sphinx.ext.viewcode",    # Adds links to highlighted source code
+    "sphinx.ext.intersphinx", # Links to external documentation (e.g., Python standard library)
+    "sphinx.ext.autosummary", # Generates stub files for autodoc automatically
 ]
+
+# --- Autodoc Customisation ---
+# Controls how members (functions, classes) are ordered in the docs
+autodoc_member_order = "bysource"  # Options: 'alphabetical', 'bysource', 'groupwise'
+
+# Default flags used by all autodoc directives (e.g., show member type hints)
+autodoc_typehints = "description"  # Options: 'signature', 'description', 'none', 'both'
+
+# Automatically mock imports that might not be installed in the readthedocs/build environment (e.g., C extensions or heavy libraries)
+autodoc_mock_imports = []
+
+# --- Intersphinx Mapping ---
+# Allows linking to Python's built-in types and other libraries
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
+
 html_theme = "sphinx_rtd_theme"
+
+autosummary_generate = True
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,35 @@
+Welcome to fbscatnet's Documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+API Reference
+=============
+
+This section documents the core modules of the ``fbscatnet`` package.
+
+Generate Bank
+-------------
+
 .. automodule:: fbscatnet.generate_bank
    :members:
+   :undoc-members:
+   :show-inheritance:
+
+Scatnet
+-------
 
 .. automodule:: fbscatnet.scatnet
    :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+Indices and Tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
