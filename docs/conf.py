a57ed6323cf84efbc5d44ea769f86a1489626cda
Smee18 <marcel.venturotti@icloud.com>
2026-07-31 11:18:10 +0200

    Revert "Dynamic version tracking"
    This reverts commit abe6f7630d1e7832fb07cf9d488d2d4be5a960f1.

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,10 @@
 import os
 import sys
-from importlib.metadata import PackageNotFoundError, version
 
 project = "fbscatnet"
 copyright = "2026, Marcel Venturotti"
 author = "Marcel Venturotti"
-try:
-    release = version("fbscatnet")
-except PackageNotFoundError:
-    release = "0.0.0"
+release = "0.1.0"
 
 sys.path.insert(0, os.path.abspath("../src"))
 
